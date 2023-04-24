ee630e0cbc4df86c5244a29bf53dcadc425826ce
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-04-24 16:08:21 +0300

    task9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const object = {
+        email: event.currentTarget.email.value,
+        password: event.currentTarget.password.value
+    };
+    
+    if (object.email.length === 0 || object.password.length === 0)
+        alert("Trebuie completate toate campurile !");
+    else
+        console.log(object);
+    form.reset(); 
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const changeBtn = document.querySelector(".change-color");
+
+changeBtn.addEventListener('click', () => {
+    
+    document.getElementsByTagName("body")[0].style.backgroundColor = "" + getRandomHexColor();
+    
+  });
